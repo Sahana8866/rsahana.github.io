e894707d9aac18a0ffeca09167826c973a2b05df
Sahana8866 <rsahana8310@gmail.com>
2024-12-26 16:53:36 +0530

    Update bellquick.cpp

--- bellquick.cpp
+++ bellquick.cpp
@@ -4,103 +4,134 @@
 
 using namespace std;
 
-vector<int> bellf(int V, vector<vector<int>> &edges, int source, vector<int>& parent) {
-    vector<int> dist(V, 1e8);
-    int u, v, wt;
-    dist[source] = 0;
+vector<int> bellf(int V,vector<vector<int>> &edges,int source,vector<int>&parent)
+{
+    vector<int>dist(V,1e8);
+    int u,v,wt;
+    dist[source]=0;
 
-    for (int i = 1; i < V; i++) {
-        for (vector<int> e : edges) {
-            u = e[0];
-            v = e[1];
-            wt = e[2];
-            if (dist[u] != 1e8 && dist[v] > (dist[u] + wt)) {
-                dist[v] = dist[u] + wt;
-                parent[v] = u;
+    for(int i=1; i<V; i++)
+    {
+        for(vector<int> e:edges)
+        {
+            u=e[0];
+            v=e[1];
+            wt=e[2];
+            if(dist[u]!=1e8 && dist[v]>(dist[u]+wt))
+            {
+                dist[v]=dist[u]+wt;
+                parent[v]=u;
             }
         }
     }
-
-    if (dist[u] != 1e8 && dist[v] > (dist[u] + wt)) {
+    if(dist[u]!=1e8 && dist[v]>(dist[u]+wt))
+    {
         return {};
     }
-    else {
+    else
         return dist;
+}
+
+int part(vector<pair<int,int>>&din,int l,int r)
+{
+    int p = din[l].second;
+    int i=l,j=r+1;
+
+    do
+    {
+        do
+        {
+            i++;
+
+        }
+        while(din[i].second<p);
+        do
+        {
+            j--;
+
+        }
+        while(j>=0 && din[j].second > p);
+
+        swap(din[i],din[j]);
+
+    }
+    while(i<j);
+
+    swap(din[i],din[j]);
+    swap(din[l],din[j]);
+    return j;
+}
+
+void quicksort(vector<pair<int,int>>&din,int l,int r)
+{
+    if(l<r)
+    {
+        int s = part(din,l,r);
+        quicksort(din,l,s-1);
+        quicksort(din,s+1,r);
     }
 }
 
-void path(int vertex, vector<int>& parent) {
-    stack<int> p;
-    while (vertex != -1) {
+void path(int vertex,vector<int>&parent)
+{
+    stack<int>p;
+    while(vertex!=-1)
+    {
         p.push(vertex);
         vertex = parent[vertex];
     }
-    while (!p.empty()) {
+    while(!p.empty())
+    {
         cout << p.top();
         p.pop();
-        if (!p.empty()) cout << " -> ";
+        if(!p.empty())
+            cout << " -> ";
     }
 }
 
-int partition(vector<pair<int, int>>& distWithIndex, int low, int high) {
-    int pivot = distWithIndex[high].second;
-    int i = low - 1;
-    for (int j = low; j < high; j++) {
-        if (distWithIndex[j].second <= pivot) {
-            i++;
-            swap(distWithIndex[i], distWithIndex[j]);
-        }
-    }
-    swap(distWithIndex[i + 1], distWithIndex[high]);
-    return i + 1;
-}
-
-void quickSort(vector<pair<int, int>>& distWithIndex, int low, int high) {
-    if (low < high) {
-        int pi = partition(distWithIndex, low, high);
-        quickSort(distWithIndex, low, pi - 1);
-        quickSort(distWithIndex, pi + 1, high);
-    }
-}
-
-int main() {
+int main()
+{
     vector<vector<int>> edges = {{0, 1, 6}, {1, 2, 2}, {1, 3, 5}, {2, 4, 2}, {3, 4, 1}, {0, 4, 10}};
     int V = 5, source = 0, i = 0, s = 0;
-    vector<int> parent(V, -1);
-    vector<int> dist = bellf(V, edges, source, parent);
+    vector<int>parent(V,-1);
+    vector<int>dist = bellf(V,edges,source,parent);
 
-    if (dist.empty()) {
+    if(dist.empty())
         cout << "Routes lead to a negative weighted cycle!\n";
-    } else {
-        cout << "Locations and their distances from Source (Depot) before sorting:\n\n";
-        for (i = 0; i < V; i++) {
-            cout << "Location " << i << " : " << dist[i] << "\n";
+
+    else
+    {
+        cout << "shortest distances (unsorted) from source 0 :\n\n";
+        for(int i=0; i<V; i++)
+        {
+            cout << "Location " << i << " : " << dist[i] << endl;
+            s+=dist[i];
         }
 
-        vector<pair<int, int>> distWithIndex;
-        for (i = 0; i < V; i++) {
-            distWithIndex.push_back({i, dist[i]});
+        vector<pair<int,int>>din;
+        for(int i=0; i<V; i++)
+        {
+            din.push_back({i,dist[i]});
         }
 
-        quickSort(distWithIndex, 0, distWithIndex.size() - 1);
+        quicksort(din,0,din.size()-1);
 
-        cout << "\nLocations and their distances from Source (Depot) after sorting:\n\n";
-        for (auto& loc : distWithIndex) {
-            cout << "Location " << loc.first << " : " << loc.second << "\n";
+        cout << "\nshortest distances (sorted) from source 0 :\n\n";
+        for(auto&loc:din)
+        {
+            cout << "Location " << loc.first << " : " << loc.second << endl;
         }
 
-        cout << "\nOptimized Paths:\n";
-        for (auto& loc : distWithIndex) {
+        cout << "\nOptimized Paths :\n\n";
+        for(auto&loc:din)
+        {
             cout << "Location " << loc.first << " : ";
-            path(loc.first, parent);
+            path(loc.first,parent);
             cout << "\n";
         }
 
-        for (auto& loc : distWithIndex) {
-            s += loc.second;
-        }
-        cout << "\nTotal Distance Travelled: " << s << endl;
+        cout << "\nTotal distance travelled : " << s << endl;
+
     }
     return 0;
 }
-
