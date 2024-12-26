63d19ef8d3de9a3686e1b6b1819df5000ce133b9
Sahana8866 <rsahana8310@gmail.com>
2024-12-27 01:14:37 +0530

    Update dym_bellf.cpp

--- dym_bellf.cpp
+++ dym_bellf.cpp
@@ -1,1 +1,52 @@
+#include<iostream>
+#include<vector>
+#include<stack>
 
+using namespace std;
+
+vector<int> bellf(int V,vector<vector<int>> &edges,int source,vector<int>&parent)
+{
+    vector<int>dist(V,1e8);
+    int u,v,wt;
+    dist[source]=0;
+
+    for(int i=1; i<V; i++)
+    {
+        for(vector<int> e:edges)
+        {
+            u=e[0];
+            v=e[1];
+            wt=e[2];
+            if(dist[u]!=1e8 && dist[v]>(dist[u]+wt))
+            {
+                dist[v]=dist[u]+wt;
+                parent[v]=u;
+            }
+        }
+    }
+    if(dist[u]!=1e8 && dist[v]>(dist[u]+wt))
+        return {};
+    return dist;
+
+}
+
+void paths(int vertex,vector<int>&parent)
+{
+    stack<int>p;
+    while(vertex!=-1)
+    {
+        p.push(vertex);
+        vertex = parent[vertex];
+    }
+
+    while(!p.empty())
+    {
+        cout << p.top();
+        p.pop();
+
+        if(!p.empty())
+        {
+            cout << " -> ";
+        }
+    }
+}
