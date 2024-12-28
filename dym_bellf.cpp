afb8ece4e2f351243d653c491938121ba8e7635d
Sahana8866 <rsahana8310@gmail.com>
2024-12-28 22:36:25 +0530

    Update dym_bellf.cpp

--- dym_bellf.cpp
+++ dym_bellf.cpp
@@ -50,3 +50,34 @@ void paths(int vertex,vector<int>&parent)
         }
     }
 }
+
+void bellmanford_4()
+{
+    vector<vector<int>> edges = {{0,1,5},{1,2,1},{1,3,2},{2,4,1},{4,3,-1}};
+    int V = 5, source = 0, i = 0, s = 0;
+    vector<int>parent(V,-1);
+    vector<int>dist = bellf(V,edges,source,parent);
+
+    if(dist.empty())
+        cout << "Routes lead to a negative weighted cycle!\n";
+
+    else
+    {
+        cout << "Shortest Path from source 0 : \n";
+        for(int i=0; i<V; i++)
+        {
+            cout << "Location " << i << " : " << dist[i] << endl;
+            s+=dist[i];
+        }
+        cout << "\n\nOptimized paths : \n" << endl;
+
+        for(int i=0; i<V; i++)
+        {
+            cout << "Location " << i << " : ";
+            paths(i,parent);
+            cout << endl;
+        }
+        cout << "\nTotal distance travelled = " << s << endl;
+    }
+}
+
