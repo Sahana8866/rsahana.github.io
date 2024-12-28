fb0135a4c38e6731b618e38a709d607e87ea10b0
Sahana8866 <rsahana8310@gmail.com>
2024-12-28 22:40:56 +0530

    Update mergedcode.cpp

--- mergedcode.cpp
+++ mergedcode.cpp
@@ -1,29 +1,158 @@
 #include<iostream>
 #include<vector>
 #include<stack>
+#include<string>
+#include <unordered_map>
+#include<math.h>
+#include <bits/stdc++.h>
+#define INF 99999
+
 
 using namespace std;
 
-int visited[10],v=8,m[10][10]= {{0,0,1,0,0,0,0,0},{0,0,1,1,0,0,0,0},{1,1,0,1,0,0,0,0},{0,1,1,0,1,1,0,0},{0,0,0,1,0,1,1,0},{0,0,0,1,1,0,1,0},{0,0,0,0,1,1,0,1},{0,0,0,0,0,0,1,0}};
+int visited[10],v=8,m[10][10]= {{0,0,1,0,0,0,0,0},
+    {0,0,1,1,0,0,0,0},
+    {1,1,0,1,0,0,0,0},
+    {0,1,1,0,1,1,0,0},
+    {0,0,0,1,0,1,1,0},
+    {0,0,0,1,1,0,1,0},
+    {0,0,0,0,1,1,0,1},
+    {0,0,0,0,0,0,1,0}
+};
 int path[10],j=0;
+const int vert = 4;
+unordered_map<string,string> impactmap;
 
-void dfs(int m[10][10],int v,int source)
+// --------------2----------
+
+void changes(unordered_map<string,string>&impactmap)
 {
-    visited[source]=1;
-    for(int i=0; i<v; i++)
+    int n;
+    string s;
+    cout << "Enter zone number, and new severity\n";
+    cin >> n;
+    cin.ignore();
+    getline(cin,s);
+    string zone = "zone "+to_string(n);
+    auto it=impactmap.find(zone);
+    if(it!=impactmap.end())
     {
-        if(m[source][i]==1 && visited[i]==0)
+        impactmap[zone]=s;
+    }
+    else
+        cout << "Zone not found\n";
+}
+
+void deletezone(unordered_map<string,string>&impactmap)
+{
+    int n;
+    cout << "Enter Zone number\n";
+    cin >> n;
+    string zone = "zone " + to_string(n);
+    auto it = impactmap.find(zone);
+    if (it != impactmap.end())
+    {
+        impactmap.erase(it);
+        cout << "Zone deleted\n";
+    }
+    else
+        cout << "Zone not found\n";
+}
+
+void searchzone(unordered_map<string,string>&impactmap)
+{
+    int n;
+    cout << "Enter Zone number\n";
+    cin >> n;
+    string zone = "zone "+to_string(n);
+    auto it = impactmap.find(zone);
+    if (it != impactmap.end())
+    {
+        cout << "Zone found\n";
+        cout << zone << " : " << impactmap[zone] << endl;
+    }
+    else
+        cout << "Zone not found\n";
+}
+
+void hashingdisplay(unordered_map<string,string>&impactmap)
+{
+    if(impactmap.empty())
+    {
+        cout << "No zones available\n";
+    }
+    for(auto &pair:impactmap)
+    {
+        cout << pair.first << " : " << pair.second << endl;
+    }
+    cout << endl;
+}
+
+void addzone(unordered_map<string,string>&impactmap)
+{
+    cout << "Enter zone number and severity level\n";
+    int n;
+    string s;
+    cin >> n;
+    cin.ignore();
+    getline(cin,s);
+    string newzone = "zone " + to_string(n);
+    impactmap[newzone] = s;
+    cout << "New zone added :)\n\n";
+}
+
+void hashing_2(int *c)
+{
+    if(*c==0)
+    {
+        impactmap["zone 1"] = "High";
+        impactmap["zone 2"] = "Medium";
+        impactmap["zone 3"] = "Low";
+        impactmap["zone 4"] = "Low";
+    }
+
+    cout << "Impacted areas and their severity level : \n\n";
+    hashingdisplay(impactmap);
+    int ch,zone;
+
+    while(1)
+    {
+        cout << "\n\nWould you like to make any adjustments?\n0. None\n1. Delete zone\n2. Change Severity\n3. Display\n4. Add a zone\n5. Search for a zone\n\n";
+        cin >> ch;
+        switch(ch)
         {
-            cout << i << " ";
-            for(int j=0; j<v; j++)
+        case 0:
+            return;
+            break;
+        case 1:
+            if(impactmap.empty())
             {
-                if(m[j][i]==1)
-                    path[i]+=1;
+                cout << "No zones available\n";
             }
-            dfs(m,v,i);
+            else
+                deletezone(impactmap);
+            break;
+        case 2:
+            changes(impactmap);
+            break;
+        case 3:
+            hashingdisplay(impactmap);
+            break;
+        case 4:
+            addzone(impactmap);
+            break;
+        case 5:
+            searchzone(impactmap);
+            break;
+        default :
+            cout << "Invalid Choice\n";
+            break;
         }
     }
 }
+
+
+// -----------4-------------
 
 vector<int> bellf(int V,vector<vector<int>> &edges,int source,vector<int>&parent)
 {
@@ -72,47 +201,7 @@ void paths(int vertex,vector<int>&parent)
     }
 }
 
-void heapify( vector<pair<int,int>>&h,int &n)
-{
-    int i,j,k;
-    pair<int,int> v;
-    bool heap;
-    for(i=n/2; i>=1; i--)
-    {
-        k=i;
-        v=h[k];
-        heap=false;
-        while(!heap && 2*k<=n)
-        {
-            j=2*k;
-            if(j<n && h[j].second<h[j+1].second)
-                j=j+1;
-            if(v.second>=h[j].second)
-                heap=true;
-            else
-            {
-                h[k]=h[j];
-                k=j;
-            }
-        }
-        h[k]=v;
-    }
-
-}
-
-void heapsort(vector<pair<int,int>>&h,int &n, vector<pair<int,int>>&d)
-{
-    while(h.size()!=0)
-    {
-        d.push_back(h[1]);
-        swap(h[1],h[n]);
-        h.pop_back();
-        n--;
-        heapify(h,n);
-    }
-}
-
-int main()
+void bellmanford_4()
 {
     vector<vector<int>> edges = {{0,1,5},{1,2,1},{1,3,2},{2,4,1},{4,3,-1}};
     int V = 5, source = 0, i = 0, s = 0;
@@ -140,8 +229,29 @@ int main()
         }
         cout << "\nTotal distance travelled = " << s << endl;
     }
+}
+void dfs(int m[10][10],int v,int source)
+{
+    visited[source]=1;
+    for(int i=0; i<v; i++)
+    {
+        if(m[source][i]==1 && visited[i]==0)
+        {
+            cout << i << " ";
+            for(int j=0; j<v; j++)
+            {
+                if(m[j][i]==1)
+                    path[i]+=1;
+            }
+            dfs(m,v,i);
+        }
+    }
+}
 
 
+void dfs_10()
+{
+    int i=0,source=0;
     for(int i=0; i<v; i++)
     {
         visited[i]=0;
@@ -152,7 +262,6 @@ int main()
     cout << source << " ";
     dfs(m,v,source);
 
-    i=0;
     for(int j=0; j<v; j++)
     {
         if(m[j][i]==1)
@@ -164,34 +273,46 @@ int main()
     {
         cout << i << "\t\t" << path[i] << "\n";
     }
+}
 
-    vector<pair<int,int>>h(1);
-    int n=5,l=5;
+int main()
+{
 
-    h.push_back({1,10});
-    h.push_back({2,20});
-    h.push_back({3,15});
-    h.push_back({4,30});
-    h.push_back({5,40});
+    int ch,c=0;
 
+    while(1)
 
-    heapify(h,n);
-
-    cout << "\n\nConsidering the input as the 'safety bar' : higher the value, higher the risk\n";
-    cout << "\nZones before prioritization\n";
-
-    for(int i=1; i<=l; i++)
     {
-        cout << "Location : " << h[i].first << " , Risk " << h[i].second << "\n";
+        cout << "\n\n0.  Exit\n1. Disaster Impact Mapping (Hashing)\n";
+        cout << "2.  Dynamic Resource Allocation (Bellman Ford)\n";
+        cout << "3. Critical Infrastructure Identification (DFS)";
+        cin >> ch;
+
+        if (ch==1)
+        {
+            hashing_2(&c);
+            c=1;
+        }
+        else if (ch==2)
+        {
+            bellmanford_4();
+
+        }
+        else if (ch==3)
+        {
+            dfs_10();
+
+        }
+        else if (ch==0)
+        {
+            cout << "Thank you for your time! This System is always at your service!\nExiting...\n";
+            exit(0);
+        }
+        else
+        {
+            cout << "Invalid Choice, please choose among the above options\n";
+        }
+
     }
-    cout << "\n\nZones after prioritization\n";
-
-    vector<pair<int,int>>d(1);
-    heapsort(h,n,d);
-
-    for(int i=1; i<=l; i++)
-    {
-        cout << "Location : " << d[i].first << " , Risk " << d[i].second << "\n";
-    }
-
+    return 0;
 }
